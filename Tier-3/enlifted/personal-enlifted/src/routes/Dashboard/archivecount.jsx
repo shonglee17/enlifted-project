b51df4accefc8f79f8500f98e2ed269df0038614
henrisoho <bakingwithmymother@gmail.com>
2023-03-08 10:17:04 -0600

    grid styling code

--- Tier-3/enlifted/personal-enlifted/src/routes/Dashboard/archivecount.jsx
+++ Tier-3/enlifted/personal-enlifted/src/routes/Dashboard/archivecount.jsx
@@ -1,53 +1,33 @@
-import { Box, TextField } from '@mui/material'
+import { Box, TextField, Typography } from '@mui/material'
 import { borderRadius } from '@mui/system'
 import React from 'react'
 
 export default function ArchiveCount() {
 
     return (
-      <Box
-        sx={{
-          position: 'absolute',
-          width: '389px',
-          height: '287px',
-          left: '923px',
-          top: '800px',
-          boxShadow: 3,
-          borderRadius: 10,
-          backgroundColor: '#6B85FB'
-        }}>
-        <p
-          style={{
-            position: 'absolute',
-            left: '6.43%',
-            right: '47.3%',
-            top: '8.71%',
-            bottom: '83.28%',
-            fontWeight: 700,
-            fontSize: '20px',
-            lineHeight: '23px',
-            letterSpacing: '0.05em',
-            color: '#000000'
-          }}
-        >
-          ARCHIVE FLOWS
-        </p>
-        <p
-                  style={{
-            position: 'absolute',
-            left: '39.33%',
-            right: '39.59%',
-            top: '36.24%',
-            bottom: '35.89%',
-            fontWeight: 400,
-            fontSize: '70px',
-            lineHeight: '0px',
-            letterSpacing: '0.05em',
-            color: '#FFFFFF'
-          }}
-        >
-          25
-        </p>
+          <Box width ="100%" m='0 10px'>
+      <Box display='flex' >
+        <Box m='0 -20px 20px 0%'>
+          <Typography
+          variant='h10'
+          fontWeight='medium'
+          color='White'
+          >
+            Archive Flows
+          </Typography>
+        </Box>
+        </Box>
+        <Box m='0 20px 20px 50%'>
+        <Box>
+          <Typography
+          variant='h4'
+          fontWeight='bold'
+          color='White'
+          >
+            10
+          </Typography>
+        </Box>
       </Box>
+    </Box>
     )
 }
---
+++ Tier-3/enlifted/personal-enlifted/src/routes/Dashboard/createflow.jsx
@@ -0,0 +1,8 @@
+import { Box } from '@mui/material'
+import React from 'react'
+
+export default function CreateFlow() {
+  return (
+<h1>creative flow</h1>
+  )
+}
